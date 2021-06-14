0f92ebdeb86ca996e001818ed9fd540bb1ceeb8c
bariqhibat <bariqhibat@gmail.com>
2021-06-14 12:10:46 +0800

    simple http web service

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func main(){
-	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request){
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello world!")
-		d, _ := ioutil.ReadAll(r.Body)
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			// rw.WriteHeader(http.StatusBadRequest)
+			// rw.Write([]byte("Oops"))
+			return
+		}
 
 		log.Printf("Data %s\n", d)
+
+		fmt.Fprintf(rw, "Hello %s", d)
 	})
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request){
+	http.HandleFunc("/go odbye", func(http.ResponseWriter, *http.Request) {
 		log.Println("Goodbye world")
 	})
 
